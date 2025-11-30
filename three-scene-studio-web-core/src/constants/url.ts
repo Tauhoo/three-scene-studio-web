b169564d154572a4e9d9902e9511ae470b0c1f10
Tauhoo <tauhoo.wa@gmail.com>
2025-11-30 18:41:46 +0700

    set up discord link

--- three-scene-studio-web-core/src/constants/url.ts
+++ three-scene-studio-web-core/src/constants/url.ts
@@ -2,5 +2,5 @@ export const url = {
   EDITOR: 'https://editor.three-scene-studio.app',
   DOCUMENT:
     'https://three-scene-studio.notion.site/Three-Scene-Studio-document-29c39cb8619980dfaf77da5333d25d13',
-  DISCORD: '',
+  DISCORD: 'https://discord.gg/79X7rVRRyU',
 }
